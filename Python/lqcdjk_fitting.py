Fixed some bugs affecting form factor calculation

Python/lqcdjk_fitting.py
@@ -55,7 +55,7 @@ def fitPlateau( data, err, start, end ):
                                         w=err[ start \
                                                : rangeEnd + 1 ] ** -1,
                                         full=True )
-        
+
     # End loop over bin
 
     chiSq = chiSq / dof
@@ -609,11 +609,11 @@ def twoStateFit_twop( twop, rangeStart, rangeEnd, E_guess, T,
     return fit, chiSq
 
 
-def twoStateFit_twop_diffusionRelation( twop,
-                                        rangeStart, rangeEnd,
-                                        E_ground,
-                                        pSq, L,
-                                        mpi_confs_info ):
+def twoStateFit_twop_dispersionRelation( twop,
+                                         rangeStart, rangeEnd,
+                                         E_ground,
+                                         pSq, L,
+                                         mpi_confs_info ):
 
     comm = mpi_confs_info[ 'comm' ]
     rank = mpi_confs_info[ 'rank' ]
@@ -645,11 +645,6 @@ def twoStateFit_twop_diffusionRelation( twop,
 
     # Find fit parameters of mean values to use as initial guess
 
-    #c0 = 10 ** -3
-    #c1 = 10 ** -3
-    #E0 = 0.1
-    #E1 = 1.0
-
     E0 = pq.energy( E_ground, pSq, L )
 
     tsink = np.arange( rangeStart, rangeEnd + 1 )
@@ -668,36 +663,47 @@ def twoStateFit_twop_diffusionRelation( twop,
 
         E0_avg = np.average( E0, axis=0 )
     
-        c0 = [ 0.0, 10**-2 ]
-        c1 = [ 0.0, 10**-2 ]
-        E1 = [ E0_avg + 0.2, 10.0 ]
+        c0 = 10 ** -3
+        c1 = 10 ** -3
+        E1 = E0_avg + 0.2
+
+        #c0 = [ 0.0, 10**-2 ]
+        #c1 = [ 0.0, 10**-2 ]
+        #E1 = [ E0_avg + 0.2, 10.0 ]
     
         fitParams = np.array( [ c0, c1, E1 ] )
 
         #leastSq_avg = least_squares( twoStateErrorFunction_twop, fitParams,
         #                             args = ( tsink, T, twop_avg, twop_err ),
         #                             method="lm" )
-        #leastSq_avg = minimize( twoStateCostFunction_twop, fitParams, \
-        #                        args = ( tsink, T, twop_avg, twop_err ), \
-        #                        method="BFGS" )
-        leastSq_avg = differential_evolution( twoStateCostFunction_twop_diffRel, 
-                                              fitParams,
-                                              ( E0_avg, tsink, T, 
-                                                twop_avg, 
-                                                twop_err ),
-                                              tol=0.01 )
+        #leastSq_avg = differential_evolution( twoStateCostFunction_twop_dispRel, 
+        #                                      fitParams,
+        #                                      ( E0_avg, tsink, T, 
+        #                                        twop_avg, 
+        #                                        twop_err ),
+        #                                      tol=0.01 )
         
-        #fitParams = leastSq_avg.x
-        fitParams = np.array( [ [ max( leastSq_avg.x[ 0 ] - 10**-4, 0.0 ), 
-                                  leastSq_avg.x[ 0 ] + 10**-4 ],
-                                [ max( leastSq_avg.x[ 1 ] - 10**-4, 0.0 ),
-                                  leastSq_avg.x[ 1 ] + 10**-4 ],
-                                [ max( leastSq_avg.x[ 2 ] - 0.1, 0.0 ),
-                                  leastSq_avg.x[ 2 ] + 0.1 ] ] )
-
+        #fitParams = np.array( [ [ max( leastSq_avg.x[ 0 ] - 10**-4, 0.0 ), 
+        #                          leastSq_avg.x[ 0 ] + 10**-4 ],
+        #                        [ max( leastSq_avg.x[ 1 ] - 10**-4, 0.0 ),
+        #                          leastSq_avg.x[ 1 ] + 10**-4 ],
+        #                        [ max( leastSq_avg.x[ 2 ] - 0.1, 0.0 ),
+        #                          leastSq_avg.x[ 2 ] + 0.1 ] ] )
+        
+        leastSq_avg = minimize( twoStateCostFunction_twop_dispRel, 
+                                fitParams,
+                                args = ( E0_avg, tsink, T, 
+                                         twop_avg, 
+                                         twop_err ),
+                                tol=0.01,
+                                method="BFGS" )
+        
+        fitParams = leastSq_avg.x
+        
     else:
 
-        fitParams = np.zeros( ( 3, 2 ) )
+        #fitParams = np.zeros( ( 3, 2 ) )
+        fitParams = np.zeros( 3 )
 
     comm.Bcast( fitParams, root=0 )
 
@@ -710,16 +716,19 @@ def twoStateFit_twop_diffusionRelation( twop,
         #                         args = ( tsink, T, twop_to_fit[ b, : ], \
         #                                  twop_err ), \
         #                         method="lm" )
-        #leastSq = minimize( twoStateCostFunction_twop, fitParams, \
-        #                    args = ( tsink, T, twop_to_fit[ b, : ], 
-        #                             twop_err ), \
-        #                    method="BFGS" )
-        leastSq = differential_evolution( twoStateCostFunction_twop_diffRel, 
-                                          fitParams,
-                                          ( E0[ b ], tsink, T, 
-                                            twop_to_fit[ b, : ], 
-                                            twop_err ),
-                                          tol=0.0001 )
+        #leastSq = differential_evolution( twoStateCostFunction_twop_dispRel, 
+        #                                  fitParams,
+        #                                  ( E0[ b ], tsink, T, 
+        #                                    twop_to_fit[ b, : ], 
+        #                                    twop_err ),
+        #                                  tol=0.0001 )
+        leastSq = minimize( twoStateCostFunction_twop_dispRel, 
+                            fitParams,
+                            args = ( E0[ b ], tsink, T, 
+                                     twop_to_fit[ b, : ], 
+                                     twop_err ),
+                            tol=0.0001,
+                            method="BFGS" )
 
         fit_loc[ ib ] = leastSq.x
         chiSq_loc[ ib ] = leastSq.fun
@@ -1142,11 +1151,11 @@ def twoStateCostFunction_twop( fitParams, tsink, T, twop, sigma ):
         return
 
     
-def twoStateCostFunction_twop_diffRel( fitParams, E0, tsink, T, twop, sigma ):
+def twoStateCostFunction_twop_dispRel( fitParams, E0, tsink, T, twop, sigma ):
 
     if sigma.ndim == 1: # sigma is standard deviations
 
-        return np.sum( twoStateErrorFunction_twop_diffRel( fitParams, 
+        return np.sum( twoStateErrorFunction_twop_dispRel( fitParams, 
                                                            E0,
                                                            tsink, 
                                                            T, twop, 
@@ -1154,7 +1163,7 @@ def twoStateCostFunction_twop_diffRel( fitParams, E0, tsink, T, twop, sigma ):
     
     elif sigma.ndim == 2: # sigma is inverse covariant matrix
 
-        r = twoStateResidual_twop_diffRel( fitParams, E0, tsink, T, twop )
+        r = twoStateResidual_twop_dispRel( fitParams, E0, tsink, T, twop )
 
         return r.T @ sigma @ r
 
@@ -1186,7 +1195,7 @@ def twoStateErrorFunction_twop( fitParams, tsink, T, twop, twop_err ):
     return twopErr
     
 
-def twoStateErrorFunction_twop_diffRel( fitParams, E0, tsink, T, twop, twop_err ):
+def twoStateErrorFunction_twop_dispRel( fitParams, E0, tsink, T, twop, twop_err ):
 
     c0 = fitParams[ 0 ]
     c1 = fitParams[ 1 ]
@@ -1208,7 +1217,7 @@ def twoStateResidual_twop( fitParams, tsink, T, twop ):
     return np.array( twoStateTwop( tsink, T, c0, c1, E0, E1 ) - twop )
     
 
-def twoStateResidual_twop_diffRel( fitParams, E0, tsink, T, twop ):
+def twoStateResidual_twop_dispRel( fitParams, E0, tsink, T, twop ):
 
     c0 = fitParams[ 0 ]
     c1 = fitParams[ 1 ]

Python/physQuants.py
@@ -32,7 +32,7 @@ def twopFit( c0, E0, t ):
     #return c0 * np.exp( -E0 * t ) + c1 * np.exp( -E1 * t )
 
 
-def kineFactor_GE_GM( ratio_err, particle, mEff, Q, L ):
+def kineFactor_GE_GM( ratio_err, particle, flavor, mEff, Q, L ):
 
     # ratio_err[ Q, r ]
     # mEff[ b ]
@@ -76,17 +76,30 @@ def kineFactor_GE_GM( ratio_err, particle, mEff, Q, L ):
                 
             else:
 
-                # CJL: Should 2nd-4th elements have negative sign?
+                if particle == "pion":
+
+                    chargeSign = 1.0
+
+                elif particle == "kaon" and flavor == "u":
+        
+                    chargeSign = 1.0
+
+                elif particle == "kaon" and flavor == "s":
+                
+                    chargeSign = -1.0
 
                 kineFactor[ b, q ] = [ [ ( energy( mEff[ b ],
                                                    Qsq, L ) \
                                            + mEff[ b ] ), 0 ],
-                                       [ 2.0 * np.pi / L * Q[ q, 0 ], 0 ],
-                                       [ 2.0 * np.pi / L * Q[ q, 1 ], 0 ],
-                                       [ 2.0 * np.pi / L * Q[ q, 2 ], 0 ] ] \
+                                       [ chargeSign * 2.0 * np.pi / L
+                                         * Q[ q, 0 ], 0 ],
+                                       [ chargeSign * 2.0 * np.pi / L
+                                         * Q[ q, 1 ], 0 ],
+                                       [ chargeSign * 2.0 * np.pi / L
+                                         * Q[ q, 2 ], 0 ] ] \
                     / np.repeat( ratio_err[ q ], 2).reshape( ratioNum, 2 ) \
                     / KK_meson( mEff[ b ], Qsq, L )
-                    
+                
         # End loop over Q
     # End loop over bins
 

Python/readWrite.py
@@ -409,7 +409,7 @@ def readMomentaList( corrDir, corr_template, config, particle,
     return momList
 
 
-def readMomentaTransferList( corrDir, corrTemplate, configList,
+def readMomentumTransferList( corrDir, corrTemplate, configList,
                              particle, srcNum, pSq, dataFormat, 
                              momentumTransferListFilename, 
                              mpi_info ):
@@ -472,7 +472,7 @@ def readMomentaTransferList( corrDir, corrTemplate, configList,
         else:
 
             mpi_fncs.mpiPrintError( "ERROR (readWrite." \
-                                    + "readMomentaTransferList): " \
+                                    + "readMomentumTransferList): " \
                                     + "Data format {}".format( dataFormat ) \
                                     + " not supported.", 
                                     mpi_info )
@@ -565,7 +565,7 @@ def readTwopFile( twopDir, twop_template, configList, configNum,
 
     if dataFormat == "cpu":
 
-        template = "/twop_{0}/ave{}/msq{1:0>4}/arr"
+        template = "/twop_{0}/ave{1}/msq{2:0>4}/arr"
 
         # Get p^2=0 data to determine the size of time dimension
 
@@ -573,28 +573,28 @@ def readTwopFile( twopDir, twop_template, configList, configNum,
 
         # twop0[ b, t, q ]
 
-        twop0 = getDatasets( twopDir, configList, twop_template, \
+        twop0 = getDatasets( twopDir, configList, twop_template,
                              dsetname=dataset )[:, 0, 0, ... ].real
-
-        T = twop.shape[ -2 ]
+        
+        T = twop0.shape[ -2 ]
 
         twop_loc = np.zeros( ( len( configList ),
                                QNum, T ) )
 
-        twop_loc[ :, 0, : ] = np.moveaxis( twop0, -1, -2 )
+        twop_loc[ :, 0, : ] = np.moveaxis( twop0, -1, -2 )[ :, 0, : ]
 
         # Get p^2>0 data
 
         for iqsq in range( 1, QsqNum ):
 
-            dataset = [ template.format( particle, Qsq[ iqsq ] ) ]
+            dataset = [ template.format( particle, srcNum, Qsq[ iqsq ] ) ]
 
-            twop_tmp = getDatasets( twopDir, \
-                                    configList, \
-                                    twop_template, \
+            twop_tmp = getDatasets( twopDir,
+                                    configList,
+                                    twop_template,
                                     dsetname=dataset )[:, 0, 0, ... ].real
 
-            twop[ :, Qsq_start[ iqsq ] : Qsq_end[ iqsq ] + 1, : ] \
+            twop_loc[ :, Qsq_start[ iqsq ] : Qsq_end[ iqsq ] + 1, : ] \
                 = np.moveaxis( twop_tmp, -1, -2 )
 
     elif dataFormat == "ASCII":
@@ -604,23 +604,23 @@ def readTwopFile( twopDir, twop_template, configList, configNum,
         # files are not formatted like that.
 
         T, dummy \
-            = detTimestepAndConfigNum(twopDir + \
+            = detTimestepAndConfigNum(twopDir +
                                       twop_template.replace("*", \
                                                             configList[0]))
 
         # Get 5th column of two-point files for each configuration
 
-        twop_loc = getTxtData( twopDir, \
-                           configList, \
-                           twop_template, \
-                           dtype=float).reshape( len( configList ), \
-                                                 QNum, T, 6 )[ ..., \
-                                                               4 ]
+        twop_loc = getTxtData( twopDir,
+                               configList,
+                               twop_template,
+                               dtype=float).reshape( len( configList ),
+                                                     QNum, T, 6 )[ ...,
+                                                                   4 ]
 
     else:
         
-        twop_loc = getDatasets( twopDir, configList, \
-                            twop_template, \
+        twop_loc = getDatasets( twopDir, configList,
+                            twop_template,
                             "twop" )[ :, 0, 0, :, :, 0 ]
 
         # twop_loc[ c, t, Q ]
@@ -628,7 +628,7 @@ def readTwopFile( twopDir, twop_template, configList, configNum,
 
         twop_loc = np.moveaxis( twop_loc, -1, -2 )
 
-    mpi_fncs.mpiPrint( "Read two-point functions from files " \
+    mpi_fncs.mpiPrint( "Read two-point functions from files "
                        + "in {:.3} seconds".format( time() - t0 ), 
                        mpi_info )
 
@@ -639,17 +639,17 @@ def readTwopFile( twopDir, twop_template, configList, configNum,
     twop = np.zeros( ( configNum, ) + twop_loc.shape[ 1: ] )
     
     comm.Allgatherv( twop_loc, [ twop, 
-                                 configNum_loc_list \
+                                 configNum_loc_list
                                  * np.prod( twop_loc.shape[ 1: ] ),
-                                 confOffset \
+                                 confOffset
                                  * np.prod( twop_loc.shape[ 1: ] ),
                                  MPI.DOUBLE ] )
 
     return twop
 
 
-def getMellinMomentThreep( threepDir, configList, configNum, threep_tokens, \
-                           srcNum, ts, p, particle, dataFormat, moment, \
+def getMellinMomentThreep( threepDir, configList, configNum, threep_tokens,
+                           srcNum, ts, p, particle, dataFormat, moment,
                            L, T, mpi_info, **kwargs ):
 
     # Get the relevant three-point functions 
@@ -2537,6 +2537,7 @@ def readEMFF_gpu( threepDir, threep_template,
     configNum = len( configList )
 
     dset_keywords = [ "tsink_{}".format( ts ),
+                      flav,
                       "noether" ]
 
     # threep_tmp[ conf, t, Q, curr, re/im ]
@@ -2603,8 +2604,8 @@ def readEMFormFactorFile( threepDir, threep_tokens, srcNum,
                           dataFormat, mpi_info, **kwargs ):
 
     comm = mpi_info[ 'comm' ]
-    configNum = mpi_info[ "configNum" ]
-    configNum_loc = mpi_info[ "configNum_loc" ]
+    configNum = mpi_info[ 'configNum' ]
+    configNum_loc = mpi_info[ 'configNum_loc' ]
     configNum_loc_list = mpi_info[ 'configNum_loc_list' ]
     confOffset = mpi_info[ 'confOffset' ]
 
@@ -2636,11 +2637,11 @@ def readEMFormFactorFile( threepDir, threep_tokens, srcNum,
                                                    flav )
 
                 threep_loc[ :, iflav, iproj ] = readEMFF_cpu( threepDir,
-                                                           threep_template,
-                                                           srcNum, Qsq, ts, proj,
-                                                           particle, flav, T,
-                                                           **kwargs )
-
+                                                              threep_template,
+                                                              srcNum, Qsq, ts, proj,
+                                                              particle, flav, T,
+                                                              **kwargs )
+                
             elif dataFormat == "gpu":
 
                 template = "{0}{1}{2}"
@@ -2683,8 +2684,8 @@ def readEMFormFactorFile( threepDir, threep_tokens, srcNum,
 
     if particle == "nucleon":
 
-        # threep_loc[ flav, proj, conf, Q, curr, t ]
-        # -> threep_loc[ flav, conf, Q, ratio, t ]
+        # threep_loc[ conf, flav, proj, Q, curr, t ]
+        # -> threep_loc[ conf, flav, Q, ratio, t ]
 
         # ratio   Projector Insertion
         # 0       P0 gt
@@ -2698,16 +2699,16 @@ def readEMFormFactorFile( threepDir, threep_tokens, srcNum,
         # 8       P6 gx
         # 9       P6 gy
 
-        threep_loc = np.stack ( [ threep_loc[ :, 0, :, :, 0, : ].real,
-                                  threep_loc[ :, 0, :, :, 1, : ].imag,
-                                  threep_loc[ :, 0, :, :, 2, : ].imag,
-                                  threep_loc[ :, 0, :, :, 3, : ].imag,
-                                  threep_loc[ :, 1, :, :, 2, : ].real,
-                                  threep_loc[ :, 1, :, :, 3, : ].real,
-                                  threep_loc[ :, 2, :, :, 1, : ].real,
-                                  threep_loc[ :, 2, :, :, 3, : ].real,
-                                  threep_loc[ :, 3, :, :, 1, : ].real,
-                                  threep_loc[ :, 3, :, :, 2, : ].real ],
+        threep_loc = np.stack ( [ threep_loc[ :, :, 0, :, 0, : ].real,
+                                  threep_loc[ :, :, 0, :, 1, : ].imag,
+                                  threep_loc[ :, :, 0, :, 2, : ].imag,
+                                  threep_loc[ :, :, 0, :, 3, : ].imag,
+                                  threep_loc[ :, :, 1, :, 2, : ].real,
+                                  threep_loc[ :, :, 1, :, 3, : ].real,
+                                  threep_loc[ :, :, 2, :, 1, : ].real,
+                                  threep_loc[ :, :, 2, :, 3, : ].real,
+                                  threep_loc[ :, :, 3, :, 1, : ].real,
+                                  threep_loc[ :, :, 3, :, 2, : ].real ],
                                 axis=3 )
         
     else:
@@ -2718,10 +2719,10 @@ def readEMFormFactorFile( threepDir, threep_tokens, srcNum,
         # 2       gy
         # 3       gz
 
-        threep_loc = np.stack ( [ threep_loc[ :, 0, :, :, 0, : ].real,
-                                  threep_loc[ :, 0, :, :, 1, : ].imag,
-                                  threep_loc[ :, 0, :, :, 2, : ].imag,
-                                  threep_loc[ :, 0, :, :, 3, : ].imag ],
+        threep_loc = np.stack ( [ threep_loc[ :, :, 0, :, 0, : ].real,
+                                  threep_loc[ :, :, 0, :, 1, : ].imag,
+                                  threep_loc[ :, :, 0, :, 2, : ].imag,
+                                  threep_loc[ :, :, 0, :, 3, : ].imag ],
                                 axis=3 )
 
     threep = np.zeros( ( configNum, ) \